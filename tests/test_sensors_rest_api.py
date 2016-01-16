64edae27deae8aa266599bc1f300e26273035366
Matthieu Michon <MatthieuMichon@users.noreply.github.com>
2016-01-16 22:52:30 +0100

    Create test_sensors_rest_api.py

---
+++ tests/test_sensors_rest_api.py
@@ -0,0 +1,17 @@
+import unittest
+
+"""
+tests.test_sensors_rest_api
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Main test file.
+"""
+
+class SensorsRestApiTestCases(unittest.TestCase):
+
+    def setUp(self):
+      pass
+
+
+if __name__ == '__main__':
+    unittest.main()
