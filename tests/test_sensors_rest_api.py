1726c4f3e63b48df8bc3867c1e978e41eb4e9632
Matthieu Michon <MatthieuMichon@users.noreply.github.com>
2016-01-16 23:21:55 +0100

    Update test_sensors_rest_api.py

--- tests/test_sensors_rest_api.py
+++ tests/test_sensors_rest_api.py
@@ -1,4 +1,6 @@
+import sensors_rest_api
 import unittest
+
 
 """
 tests.test_sensors_rest_api
@@ -10,10 +12,12 @@ Main test file.
 class SensorsRestApiTestCases(unittest.TestCase):
 
     def setUp(self):
-        pass
+        sensors_rest_api.app.testing = True
+        self.app = sensors_rest_api.app.test_client()
 
     def test_dummy(self):
-        pass
+        rv = self.app.get('/')
+        assert 'No entries here so far' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
